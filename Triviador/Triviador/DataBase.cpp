8977ca936ac493f84f93b4893f53b97ab1cb8f93
Andreea <andreiandreea749@gmail.com>
2022-11-25 10:06:49 +0200

    Implemented methods to create and populate a database.

--- Triviador/Triviador/DataBase.cpp
+++ Triviador/Triviador/DataBase.cpp
@@ -1,7 +1,15 @@
 #include "DataBase.h"
 
-DataBase::DataBase(std::string IP) 
-	: m_IP(IP)
+DataBase::DataBase(Storage& storage)
+	: m_db {storage}
 {
+}
 
+void populateStorage(Storage& storage)
+{
+	std::vector<User> users = {
+		User{ -1, "MilkAna",  "1234AnaLapte", "1","3","1", "2022-08-10"}
+	};
+	
+	storage.insert_range(users.begin(), users.end());
 }
--- Triviador/Triviador/DataBase.h
+++ Triviador/Triviador/DataBase.h
@@ -8,11 +8,50 @@
 #include "Utils.h"
 #include "Player.h"
 
+namespace sql = sqlite_orm;
+
+using Coords = std::pair<uint8_t, uint8_t>;
+
+struct User
+{
+	int id;
+	std::string username;
+	std::string password;
+	std::string accountCreationDate;
+	std::string score;
+	std::string playedGames, wonGames;
+};
+
+inline auto createStorage(const std::string& filename)
+{
+	return sql::make_storage(
+		//returneaza un obiect specific acestei biblioteci
+		//- obiectul este un template -> se va crea de fiecare data un obiect special
+		filename,
+		sql::make_table(
+			"Users",
+			sql::make_column("id", &User::id, sql::autoincrement(), sql::primary_key()),
+			sql::make_column("name", &User::username),
+			sql::make_column("password", &User::password),
+			sql::make_column("score", &User::score),
+			sql::make_column("playedGames", &User::playedGames),
+			sql::make_column("wonGames", &User::wonGames),
+			sql::make_column("accountCreationDate", &User::accountCreationDate)
+		)
+	);
+}
+
+using Storage = decltype(createStorage(""));
+
+void populateStorage(Storage& storage);
+
 class DataBase
 {
 public:
-	DataBase(std::string IP);
+	DataBase(Storage& storage);
 
 private:
-	std::string m_IP; 
+	std::string m_IP;
+
+	Storage& m_db;
 };
