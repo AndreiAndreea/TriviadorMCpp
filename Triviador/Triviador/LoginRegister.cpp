faa42c09e81f5c6b225f1f7e068d7f937184e5c3
MariaBiserica <mariabiserica@gmail.com>
2022-12-17 14:56:09 +0200

    Added new widget in LoginRegister.ui to get the IP and Port to later connect to the server. Added in LoginRegister class getters and methods to check the correctness of the IP and Port variables. Once the server data is correctly entered, the user will have access to the LoginRegister widget.

--- Triviador/Triviador/LoginRegister.cpp
+++ Triviador/Triviador/LoginRegister.cpp
@@ -8,11 +8,101 @@
 LoginRegister::LoginRegister()
 {
 	ui.setupUi(this);
+
+	ui.serverDataWidget->show();
+	ui.ipErrorLabel->hide();
+	ui.portErrorLabel->hide();
+
+	ui.loginRegisterWidget->hide();
 }
 
 LoginRegister::~LoginRegister()
 {
 
+}
+
+std::string LoginRegister::GetServerIP()
+{
+	return m_serverIP;
+}
+
+std::string LoginRegister::GetServerPort()
+{
+	return m_serverPort;
+}
+
+std::vector<std::string> LoginRegister::splitIP()
+{
+	int i = 0;
+	std::vector<std::string> ip;
+	auto pos = m_serverIP.find(".");
+	while ( pos != std::string::npos) 
+	{
+		ip.push_back(m_serverIP.substr(i, pos - i));
+		i = ++pos;
+		pos = m_serverIP.find(".", pos);
+	}
+	ip.push_back(m_serverIP.substr(i, m_serverIP.length()));
+	return ip;
+}
+
+bool LoginRegister::isNumber(const std::string& str)
+{
+	for (const auto& c : str)
+		if (!isdigit(c))
+			return false;
+	return true;
+}
+
+QString LoginRegister::checkServerIP()
+{
+	std::vector<std::string> ipTokens = splitIP();
+
+	if (m_serverIP.size() == 0)
+		return "No server IP inserted!";
+	else if (ipTokens.size() != 4)
+		return "Not valid IP! Not enough tokens!";
+		
+	// checks that tokens are numbers, positive and in range
+	for (const auto& str : ipTokens) {
+		if (!isNumber(str))
+			return "Not valid IP! Tokens contain non-digit values!";
+		else if (stoi(str) > 255)
+			return "Not valid IP! Token values bigger than 255 not allowed!";
+	}
+
+	return "";
+}
+
+QString LoginRegister::checkServerPort()
+{
+	if(m_serverPort.size() == 0)
+		return "No server Port inserted!";
+	else if (!isNumber(m_serverPort))
+		return "Not valid Port! Contains non-digit values!";
+
+	return "";
+}
+
+void LoginRegister::on_connectButton_released()
+{
+	m_serverIP = ui.serverIPLineEdit->text().toStdString();
+	m_serverPort = ui.serverPortLineEdit->text().toStdString();
+
+	QString ipErrorText = checkServerIP();
+	QString portErrorText = checkServerPort();
+
+	ui.ipErrorLabel->setText(ipErrorText);
+	ui.ipErrorLabel->show();
+
+	ui.portErrorLabel->setText(portErrorText);
+	ui.portErrorLabel->show();
+
+	if (ipErrorText.isEmpty() && portErrorText.isEmpty())
+	{
+		ui.serverDataWidget->hide();
+		ui.loginRegisterWidget->show();
+	}
 }
 
 void LoginRegister::on_signInButton_released()
--- Triviador/Triviador/LoginRegister.h
+++ Triviador/Triviador/LoginRegister.h
@@ -19,12 +19,24 @@ public:
 
 	//explicit Login(QWidget* parent); - Cosmin
 
-private:
+public:
+	std::string GetServerIP();
+	std::string GetServerPort();
 	
+private:
+	std::vector<std::string> splitIP();
+	bool isNumber(const std::string& str);
+
+	QString checkServerIP();
+	QString checkServerPort();
+
 private slots:
+	void on_connectButton_released();
+
 	void on_signInButton_released();
 	void on_signUpButton_released();
 
 private:
+	std::string m_serverIP, m_serverPort;
 };
 
